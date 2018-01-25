24182f9a2d14ad0875c9debdacf8c6783c5c3da8
blackout314 <blackout_lair@yahoo.com>
2018-01-25 09:47:32 +0100

    add shell utilz

---
+++ shell/colors.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+WHITE='\033[0;37m'
+BLACK='\033[0;30m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+GRAY='\033[0;37m'
+RESET='\033[0m'
+
+
+echo -e "${GREEN}User exists in passwd file${RESET}"
+echo -e "${RED}User does NOT exist in passwd file${RESET}"
+echo -e "${WHITE}User does NOT exist in passwd file${RESET}"
+echo -e "${BLACK}User does NOT exist in passwd file${RESET}"
+echo -e "${YELLOW}User does NOT exist in passwd file${RESET}"
+echo -e "${BLUE}User does NOT exist in passwd file${RESET}"
+echo -e "${MAGENTA}User does NOT exist in passwd file${RESET}"
+echo -e "${CYAN}User does NOT exist in passwd file${RESET}"
+echo -e "${GRAY}User does NOT exist in passwd file${RESET}"

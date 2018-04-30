fad9b6a8b45cb6c512ce90f01d5fa4433cf025b6
Carlo Denaro <blackout_lair@yahoo.com>
2018-04-30 13:52:42 +0200

    Update syncthing-install.sh

--- shell/syncthing-install.sh
+++ shell/syncthing-install.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+## sudo apt-get install apt-transport-https
+
+apt-get update
 apt-get install curl -y
 curl -s https://syncthing.net/release-key.txt | apt-key add -
+
 echo "deb https://apt.syncthing.net/ syncthing stable" | tee /etc/apt/sources.list.d/syncthing.list
 apt-get update
 apt-get install syncthing -y
+
+useradd -r syncthing -m -d /home/syncthing
+systemctl enable syncthing@syncthing.service
+# systemctl start syncthing@syncthing.service
+curl -I 127.0.0.1:8384

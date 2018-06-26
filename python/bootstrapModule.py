e4bb73ae8c6e58cd690f3f05e55c852e87384996
blackout314 <blackout_lair@yahoo.com>
2018-06-26 14:47:07 +0200

    adding ask_user y/n for python :camel:

--- python/bootstrapModule.py
+++ python/bootstrapModule.py
@@ -40,6 +40,20 @@ def set_easy_logger(name, verbosity, file=False):
     # finally, return the logger
     return logger
 
+def ask_user():
+    check = str(input("[ASK] Are you sure ? (Y/N) ")).lower().strip()
+    try:
+        if check[0] == 'y':
+            return True
+        elif check[0] == 'n':
+            return False
+        else:
+            print('Invalid Input')
+            return ask_user()
+    except Exception as error:
+        print("Please enter valid inputs")
+        print(error)
+        return ask_user()
 
 def options():
     parser = argparse.ArgumentParser(description='''

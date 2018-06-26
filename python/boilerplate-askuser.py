b970fd9a23d36a7107c3021f13b574535140e042
Carlo Denaro <blackout_lair@yahoo.com>
2018-06-26 14:48:38 +0200

    :camel: ask user y/n

---
+++ python/boilerplate-askuser.py
@@ -0,0 +1,15 @@
+
+def ask_user():
+    check = str(input("[ASK] Are you sure ? (Y/N) ")).lower().strip()
+    try:
+        if check[0] == 'y':
+            return True
+        elif check[0] == 'n':
+            return False
+        else:
+            print('Invalid Input')
+            return ask_user()
+    except Exception as error:
+        print("Please enter valid inputs")
+        print(error)
+        return ask_user()

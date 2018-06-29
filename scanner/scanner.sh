dd452771ccfd3d55a8a2b19e69fac630dd9904ae
blackout314 <blackout_lair@yahoo.com>
2018-06-29 16:51:54 +0200

    reverse logic

--- scanner/scanner.sh
+++ scanner/scanner.sh
@@ -5,9 +5,9 @@ while read p; do
   URL=`echo $p | awk -F' ' '{print $2}'`
   RESULT=`curl -A "Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3" -s -o /dev/null -w "%{http_code}" $1$URL`
 
-  if [ "$RESULT" == "200" ] || [ "$RESULT" == "403" ]; then
-    echo " >>> $URL $RESULT"
-  else
+  if [ "$RESULT" == "404" ] || [ "$RESULT" == "503" ]; then
     echo " ... $URL $RESULT"
+  else
+    echo " >>> $URL $RESULT"
   fi
 done <scanner.txt

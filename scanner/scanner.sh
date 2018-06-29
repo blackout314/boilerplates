529388eabf7be1e9844df971f42859089c0757aa
blackout314 <blackout_lair@yahoo.com>
2018-06-29 16:02:21 +0200

    updated list - added user agent

--- scanner/scanner.sh
+++ scanner/scanner.sh
@@ -3,7 +3,7 @@
 while read p; do
   VERB=`echo $p | awk -F' ' '{print $1}'`
   URL=`echo $p | awk -F' ' '{print $2}'`
-  RESULT=`curl -s -o /dev/null -w "%{http_code}" $1$URL`
+  RESULT=`curl -A "Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3" -s -o /dev/null -w "%{http_code}" $1$URL`
 
   if [ "$RESULT" == "200" ] || [ "$RESULT" == "403" ]; then
     echo " >>> $URL $RESULT"

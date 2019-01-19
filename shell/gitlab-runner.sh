a3c23dad6e0f9b268404031e42094cc7a8e0eed2
Carlo Denaro <818568+blackout314@users.noreply.github.com>
2019-01-19 15:13:39 +0100

    Create gitlab-runner.sh

---
+++ shell/gitlab-runner.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget -O /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
+chmod +x /usr/local/bin/gitlab-runner
+useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
+gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runne
+gitlab-runner start
+echo 'NOW YOU CAN REGISTER IT> gitlab-runner register'
